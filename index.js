d9fd5b78fee09401bcf3142d35eea03ed8744780
Andres <andres.rivas@mercadolibre.com>
2020-09-27 08:44:18 -0300

    Test connection to socketio

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 const { config } = require('./config/index');
 
@@ -7,6 +8,13 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
 
-app.listen(config.port, () => {
+
+io.on('connection', (socket) => {
+  console.log(`Tested on http://localhost:${config.port}/socket.io/?EIO=3&transport=websocket`);
+  console.log('a user connected');
+});
+
+http.listen(config.port, () => {
   console.log(`Listenig on port http://localhost:${config.port}`);
 });
+
